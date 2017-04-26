68688f52b289082b2fd3400549995cffb85a4473
Mostafa Haque <93haque@gmail.com>
2017-04-26 13:46:54 -0400

    Object pooling example scripts added

---
+++ TheMountainsOfMattness/Assets/ObjectivePool.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectivePool:MonoBehaviour{
+
+    public static Queue<GameObject> pool = new Queue<GameObject>();
+
+    public static GameObject GetFromPool()
+    {
+        GameObject bullet;
+
+        if (pool.Count > 0)
+        {
+            bullet = pool.Dequeue();
+            bullet.SetActive(true);
+
+        }else
+        {
+            bullet = Instantiate(Resources.Load("Bullet")) as GameObject;
+        }
+
+        bullet.GetComponent<Poolable>().Reset();
+
+        return bullet;
+
+   }
+    public static void AddToPool(GameObject obj){
+        obj.SetActive(false);
+        pool.Enqueue(obj);
+
+    }
+
+}
