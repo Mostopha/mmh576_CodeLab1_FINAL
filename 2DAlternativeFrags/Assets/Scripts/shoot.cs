3890ac6a371ba60c395998e3963443f7cd03461b
Mostafa Haque <93haque@gmail.com>
2017-04-30 20:56:53 -0400

    FiringBullets

---
+++ 2DAlternativeFrags/Assets/Scripts/shoot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour {
+
+    public GameObject bullet;
+    public float speed = 5.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Screen.height - Input.mousePosition.y));
+            Vector2 myPos = new Vector2(transform.position.x, transform.position.y + 1);
+            Vector2 direction = target - myPos;
+            direction.Normalize();
+            GameObject projectile = (GameObject)Instantiate(bullet, myPos, Quaternion.identity);
+            projectile.GetComponent<Rigidbody2D>().velocity = direction * speed;
+        }
+    }
+
+}
