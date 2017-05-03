1ec74faf8018d876d3b9f4d68e29e54742d70d24
Mostafa Haque <93haque@gmail.com>
2017-05-03 00:17:09 -0400

    Ammo Bar Added

--- 2DAlternativeFrags/Assets/Scripts/shoot.cs
+++ 2DAlternativeFrags/Assets/Scripts/shoot.cs
@@ -10,14 +10,40 @@ public class shoot : MonoBehaviour
 
     public float ammo = 10;
 
+
+    public float barDisplay; //current progress
+    public Vector2 pos = new Vector2(20, 40);
+    public Vector2 size = new Vector2(60, 20);
+    public Texture2D emptyTex;
+    public Texture2D fullTex;
+
+
+    void OnGUI()
+    {
+        //draw the background:
+        GUI.BeginGroup(new Rect(pos.x, pos.y, size.x, size.y));
+        GUI.Box(new Rect(0, 0, size.x, size.y), emptyTex);
+
+        //draw the filled-in part:
+        GUI.BeginGroup(new Rect(0, 0, size.x * barDisplay, size.y));
+        GUI.Box(new Rect(0, 0, size.x, size.y), fullTex);
+        GUI.EndGroup();
+        GUI.EndGroup();
+    }
+
     // Use this for initialization
     void Start()
     {
 
     }
 
+
+
     void Update()
     {
+
+        barDisplay = ammo*0.10f;
+
         if (ammo > 0)
 
         {
